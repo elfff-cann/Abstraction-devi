ebc0a3acd5f479d7e021ff9fa61ab89805954f9f
elfff-cann <elifcan382006@gmail.com>
2025-03-07 17:32:52 +0300

    Add files via upload

---
+++ ödev1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ödev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+   
+}
+interface Hareket
+{
+    void HızlıGit();
+    void DenizdeYüz();
+    void HavadaUç();
+}
+class BMW : Hareket
+{
+    public void HızlıGit()
+    {
+        Console.WriteLine("BMW çok hızlı gider . ");
+    }
+    public void DenizdeYüz()
+    {
+        Console.WriteLine("BMW denizde yüzer. ");
+    }
+    public void HavadaUç()
+    {
+        Console.WriteLine("BMW havada uçar.");
+    }
+}
+class Mercedes : Hareket
+{
+    public void HızlıGit()
+    {
+        // Mercedes hızlı gitme özelliğine sahip değil.
+    }
+    public void DenizdeYüz()
+    {
+        Console.WriteLine("Mercedes denizde yüzer.");
+    }
+    public void HavadaUç()
+    {
+        Console.WriteLine("Mercedes havada uçar.");
+    }
+}
+class Porsche : Hareket
+{
+    public void HızlıGit()
+    {
+        // Porsche hızlı gitme özelliğine sahip değil.
+    }
+    public void DenizdeYüz()
+    {
+        // Porsche denizde yüzme özelliğine sahip değil.
+    }
+    public void HavadaUç()
+    {
+        Console.WriteLine("porsche havada uçar. ");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Hareket bmw = new BMW();
+        Hareket mercedes = new Mercedes();
+        Hareket porsche = new Porsche();
+        Console.WriteLine("BMW'nin hareket özellikleri:");
+        bmw.HızlıGit();
+        bmw.DenizdeYüz();
+        bmw.HavadaUç();
+
+        Console.WriteLine("Mercedes'in hareket özellikleri:");
+        mercedes.DenizdeYüz();
+        mercedes.HavadaUç();
+
+        Console.WriteLine("Porsche'nin hareket özellikleri:");
+        porsche.HavadaUç();
+        Console.ReadLine();
+    }
+}
---
+++ ödev2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ödev2
+{
+    internal class Program
+    {
+        
+        
+        }
+    }
+
+interface Calisan
+{
+    double MaasinizNedir();
+}
+// Genek Müdür sınıfı
+class GenelMudur : Calisan
+{
+    public double MaasinizNedir()
+    {
+        return 80000;
+    }
+}
+// Müdür sınıfı
+class Mudur : Calisan
+{
+    public double MaasinizNedir()
+    {
+        return 60000;
+    }
+}
+// Programcı sınıf 
+class Programci : Calisan
+{
+    public double MaasinizNedir()
+    {
+        return 40000;
+    }
+}
+// Stajyer sınıfı
+class Stajyer : Calisan
+{
+    public double MaasinizNedir()
+    {
+        return 5000;
+    }
+}
+// Ana program sınıfı
+class Program
+{
+    static void Main()
+    {
+        List<Calisan> calisanlar = new List<Calisan>
+        {
+            new GenelMudur(),
+            new Mudur(),
+            new Programci(),
+            new Stajyer(),
+        };
+        double toplamMaas = 0;
+        foreach (Calisan calisan in calisanlar)
+        {
+            toplamMaas += calisan.MaasinizNedir();
+        }
+        Console.WriteLine("Toplam Maaş:" + toplamMaas);
+        Console.ReadLine();
+    }
+}
+            
+            
